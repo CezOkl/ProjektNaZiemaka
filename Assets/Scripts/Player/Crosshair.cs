b8fdbb9c5e6202d4fde9bc0b85982a408d348f52
Cezary Okła <cezary.okla@interia.pl>
2018-04-02 21:45:13 +0200

    Modele

--- Assets/Scripts/Player/Crosshair.cs
+++ Assets/Scripts/Player/Crosshair.cs
@@ -1,22 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Crosshair : MonoBehaviour {
 
-    public Camera cam;
+   
     public Transform crosshair;
-    public int speed;
-
-	// Use this for initialization
-	void Start () {
+    public Image crosshairImage;
+    public int crosshairDistance;
+    public int maxDistance;
+    RaycastHit hit;
+    // Use this for initialization
+    void Start () {
       
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 screenPosition = cam.WorldToScreenPoint(transform.position);
-        crosshair.transform.position = Vector3.Lerp(crosshair.transform.position, screenPosition, speed * Time.deltaTime);
-	}
+        RayCrosshair();
+        if (hit.transform != null)
+        {
+            if (hit.transform.tag =="Enemy")
+            {
+                crosshairImage.GetComponent<Image>().color = Color.red;
+            }
+            else
+            {
+                crosshairImage.GetComponent<Image>().color = Color.white;
+            }
+        }
+        else
+        {
+            crosshairImage.GetComponent<Image>().color = Color.white;
+        }
+    }
 
+    void RayCrosshair()
+    {
+    
+        Physics.Raycast(this.gameObject.transform.position, Vector3.forward, out hit, maxDistance);
+     
+        Debug.DrawRay(this.gameObject.transform.position, transform.forward*maxDistance, Color.green);
+       // Debug.Log(hit.point);
+        crosshair.position = transform.position + transform.forward * crosshairDistance;
+    }
 }
